f8b9c467f3629ccefb3b5ee62e03dfe651a82c3d
Chris Love <clove@extremewebworks.com>
2014-08-03 18:00:02 -0400

    updated getViewData to getViewInfo

--- js/dev/bakpack.js
+++ js/dev/bakpack.js
@@ -1,27 +1,14 @@
 ;
 //Backpack is a deferred content managment library with single page and mobile applications in mind
-(function (window, undefined) {
+(function(window, undefined) {
 
     "use strict";
 
-    var backpack = function (settings) {
+    var backpack = function(customSettings) {
 
-        var that = new backpack.fn.init(),
-            name, copy;
+        var that = new backpack.fn.init(customSettings);
 
-        for (name in that.settings) {
-
-            copy = settings[name];
-
-            // Prevent never-ending loop
-            if (that.settings === copy) {
-                continue;
-            }
-
-            if (copy !== undefined) {
-                that.settings[name] = copy;
-            }
-        }
+        that.settings = $.extend({}, that.settings, customSettings);
 
         return that;
     };
@@ -30,64 +17,16 @@
 
         constructor: backpack,
 
-        init: function () {
+        init: function() {
 
             return this;
         },
 
-        version: "0.0.4",
+        version: "0.0.3",
 
-        parseLocalStorage : function (key) {
-
-        var value = localStorage.getItem(key);
-
-        if (!value) {
-            return {};
-        }
-
-        return JSON.parse(value) || {};
-
-    },
-
-        getTemplates: function (remove) {
-
-            var that = this,
-                i, temp,
-                t = document.querySelectorAll("script[type='" + this.settings.templateType + "']"),
-                templates = that.parseLocalStorage("templates");
-
-            for (i = 0; i < t.length; i++) {
-
-                temp = t[i];
-
-                templates[temp.id] = temp.innerHTML.replace(/(\r\n|\n|\r)/gm, "");
-
-                if (remove) {
-
-                    if (temp.parentNode) {
-                        temp.parentNode.removeChild(temp);
-                    }
-                }
-
-            }
-
-            localStorage.setItem("templates", JSON.stringify(templates));
-
-            return templates;
-
-        },
 
         //keep
-        getTemplate: function (id) {
-
-            return "<script type='" +
-                        this.settings.templateType
-                        + "'>" + localStorage.getItem(id) + "</script>";
-
-        },
-
-        //keep
-        updateViews: function (selector) {
+        updateViews: function(selector) {
 
             var i, views = document.querySelectorAll(selector);
 
@@ -97,9 +36,8 @@
 
         },
 
-
         //keep, but modify the promise stuff, take it out 4 now
-        saveViewToStorage: function (e) {
+        saveViewToStorage: function(e) {
 
             if (typeof e === "string") { //assume this is the element id
                 e = document.getElementById(e);
@@ -119,33 +57,33 @@
         },
 
         //keep, but update
-        parseViewInfo: function (ve) {
+        parseViewInfo: function(ve) {
 
             return {
                 pageId: ve.id,
-                viewTitle: (ve.hasAttribute("data-title") ?
-                                ve.getAttribute("data-title") :
-                                this.settings.defaultTitle),
-                tranistion: (ve.hasAttribute("data-transition") ?
-                                ve.getAttribute("data-transition") :
-                                ""), //need a nice way to define the default animation
+                viewTitle: (ve.hasAttribute("spa-title") ?
+                    ve.getAttribute("spa-title") :
+                    this.settings.defaultTitle),
+                tranistion: (ve.hasAttribute("spa-transition") ?
+                    ve.getAttribute("spa-transition") :
+                    ""), //need a nice way to define the default animation
                 content: ve.outerHTML
             };
 
         },
 
         //keep
-        storeViewInfo: function (viewInfo) {
+        storeViewInfo: function(viewInfo) {
 
             viewInfo = $.extend({}, this.pageSettings, viewInfo);
 
             localStorage.setItem(this.settings.appName + "-" + viewInfo.pageId,
-                            JSON.stringify(viewInfo));
+                JSON.stringify(viewInfo));
 
         },
 
         //keep
-        getViewData: function (viewId) {
+        getViewInfo: function(viewId) {
 
             var viewData = localStorage[this.settings.appName + "-" + viewId],
                 view;
@@ -168,23 +106,17 @@
         },
 
         settings: {
-            viewSelector: ".content-pane",
+            viewSelector: ".spa-view",
             defaultTitle: "A Really Cool SPA App",
             deferredTimeKey: "lastDeferredTime",
             templateType: "text/x-mustache-template",
             currentClass: "current",
-            appName: ""
+            appName: "AppX"
         },
 
         pageSettings: {
             pageId: "",
-            url: "",
-            css: undefined,
-            js: undefined,
-            templates: "",
-            content: "",
-            defaultTemplate: "#DefaultViewTemplate"
-
+            content: ""
         }
 
     };
@@ -195,5 +127,3 @@
     return (window.backpack = backpack);
 
 })(window);
-
-
